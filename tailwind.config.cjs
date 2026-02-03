8aa5ed1a0d6fdd7500ab6feaec66506e9e3c6bc3
AlexInfl007 <vasyabaks7777@gmail.com>
2026-02-03 17:30:47 +0300

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'bg-dark': '#2E004E',
+        'panel': '#3C0068'
+      }
+    },
+  },
+  plugins: [],
+};
